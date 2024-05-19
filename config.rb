879cfe8fa828ea7047aa10b1f674c676fa2ee35d
Brad Thompson <bkt@brad-thompson.com>
2024-05-19 10:50:26 -0400

    redo all the things

--- config.rb
+++ config.rb
@@ -1,13 +1,2 @@
-require 'dotenv/load'
-require_relative 'lib/font_awesome_helper'
-ignore 'stylesheets/sass/**/*.sass'
-
-set :build_dir, 'public'
 activate :directory_indexes
-
-configure :build do
-  activate :minify_css
-  activate :minify_javascript
-end
-
-helpers FontAwesomeHelper
+activate :asset_hash
